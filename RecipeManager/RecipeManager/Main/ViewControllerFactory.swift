b21c3eef88a617207d385328aa2e2967d6a14eff
matiasdim <matiasdim3@gmail.com>
2023-02-03 11:00:00 -0500

    - Removed no needed code

--- RecipeManager/RecipeManager/Main/ViewControllerFactory.swift
+++ RecipeManager/RecipeManager/Main/ViewControllerFactory.swift
@@ -16,9 +16,6 @@ protocol ViewsFactory {
 struct ViewControllerFactory: ViewsFactory {
 
     func makeRecipesViewController(withSelectionAction action: @escaping (Int) -> Void) -> UIViewController {
-//        let recipe1 = Recipe(id: 1, title: "Test Title", image: "https://spoonacular.com/productImages/432875-312x231.jpg", time: 192, servings: 5, sourceURL: "", summary: "This is the summary of the recipe", instructions: "The instructions of the recipe comes here")
-//        
-//        let recipe2 = Recipe(id: 2, title: "Test Title 2", image: "https://upload.wikimedia.org/wikipedia/commons/c/c2/Escudo_del_Deportivo_Independiente_Medell%C3%ADn.png", time: 192, servings: 5, sourceURL: "", summary: "This is the summary of the recipe", instructions: "The instructions of the recipe comes here")
         let viewModel = RecipesViewModel(recipes: [])
         return RecipesViewController(viewModel: viewModel, selection: action)
     }
